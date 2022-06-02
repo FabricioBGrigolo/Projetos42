00c9a0fcba5c9b9307b3f501649dac786089fbf8
Fabrício <fbc.grigolo@gmail.com>
2022-06-02 17:43:39 -0300

    Script de navegação/faq/scroll

---
+++ FantasticAnimals/js/script.js
@@ -0,0 +1,98 @@
+// Navegação em Tab nas fotos dos animais
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+// Navegação em Accordion nas perguntas e respostas
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+  }
+}
+initAccordion();
+
+// Scroll suave
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    //  form alternativa
+    //   const top = section.offsetTop;
+    //   window.scrollTo({
+    //       top: top,
+    //       behavior: 'smooth',
+    //   })
+  }
+}
+initScrollSuave();
+
+// Anima as sections durante o scroll da página
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    window.addEventListener("scroll", animaScroll);
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+  }
+}
+initAnimacaoScroll();
