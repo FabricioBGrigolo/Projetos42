c983512f3bc807725b20285aafca1402083d156c
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-07-08 17:44:17 -0300

    Separando o JS em modules

---
+++ FantasticAnimals/js/modules/scroll-suave.js
@@ -0,0 +1,27 @@
+// Scroll suave
+export default function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+  
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+  
+      //  form alternativa
+      //   const top = section.offsetTop;
+      //   window.scrollTo({
+      //       top: top,
+      //       behavior: 'smooth',
+      //   })
+    }
+  }
+  
