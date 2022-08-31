c8da4d35dde59a56936f35d9cf05059d9c0b72ae
fbgrigolo <fbc.grigolo@gmail.com>
2022-08-31 17:38:23 -0300

    menu mobile refatorado

--- FantasticAnimals/js/modules/menu-mobile.js
+++ FantasticAnimals/js/modules/menu-mobile.js
@@ -1,23 +1,42 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["click", "touchstart"];
+export default class initMenuMobile {
+  constructor(menuButton, menuList, events) {
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = "active";
 
-  function openMenu() {
-    menuList.classList.toggle("active");
-    menuButton.classList.toggle("active");
+    // Define touchstart e click como argumento padrão
+    // de events caso o usuário não defina
+    if (events === undefined) {
+      this.events = ["touchstart", "click"];
+    } 
+    else {
+      this.events = events;
+    }
+ 
+    this.openMenu = this.openMenu.bind(this);
+  }
 
-    outsideClick(menuList, eventos, () => {
-      menuList.classList.remove("active");
-      menuButton.classList.remove("active");
+
+  openMenu() {
+    this.menuList.classList.toggle(this.activeClass);
+    this.menuButton.classList.toggle(this.activeClass);
+
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
     });
   }
 
-  if (menuButton) {
-    eventos.forEach((userEvent) =>
-      menuButton.addEventListener(userEvent, openMenu)
-    );
+  addMenuMobileEvents() {
+    this.events.forEach((userEvent) => this.menuButton.addEventListener(userEvent, this.openMenu));
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
   }
 }
