39091aa010031e3aff70aaf0fa1a8e704d16f856
fbgrigolo <fbc.grigolo@gmail.com>
2023-03-13 17:25:00 -0300

    Criado o header, footer e html inicial da home

---
+++ surf-spirit/theme-bones/header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<script src="https://kit.fontawesome.com/357a427c1b.js" crossorigin="anonymous"></script>
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php // wordpress head functions ?>
+		<?php wp_head(); ?>
+		<?php // end of wordpress head ?>
+
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
+
+	</head>
+
+	<body <?php body_class(); ?> >
+
+			<header class="header">
+				<div class="container">
+					<div class="header-nav">
+						<img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/Logo.png" alt=""> <!--LOGO --> 
+
+						<nav class="nav-principal">
+							<ul>
+								<li>
+									<a href="">Sobre</a>
+								</li>
+								<li>
+									<a href="">Loja</a>
+								</li>
+								<li>
+									<a href="">Notícias</a>
+								</li>
+								<li>
+									<a href="">Contato</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
---
+++ surf-spirit/theme-bones/page-inicial.php
@@ -0,0 +1,78 @@
+<?php /* Template Name: page-inicial */ ?>
+
+<?php get_header(); ?>
+
+<main class="grid-newspaper">
+	<div class="posts-left">
+		<div class="post-left-top">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+		<div class="post-left-center">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+		<div class="post-left-down">
+				<img class="post-img" src="" alt="">
+				<div class="post-description">
+					<span>5min de leitura</span>
+					<h3>Notícia Terciária</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+				</div>
+		</div>
+	</div>
+	<div class="posts-center">
+		<div class="post-main">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+		<div class="post-submain">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+	</div>
+	<div class="posts-right">
+		<div class="post-right-top">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+		<div class="post-right-center">
+			<img class="post-img" src="" alt="">
+			<div class="post-description">
+				<span>5min de leitura</span>
+				<h3>Notícia Terciária</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+			</div>
+		</div>
+		<div class="post-right-down">
+				<img class="post-img" src="" alt="">
+				<div class="post-description">
+					<span>5min de leitura</span>
+					<h3>Notícia Terciária</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitmollis ac cursus id, eleifend. mollis ac cursus id, eleifend vel odioing elit. Fusce neque risus, mollis Fusce neque risus, mollis ..</p>
+				</div>
+		</div>
+	</div>
+</main>
+
+<?php get_footer(); ?>
